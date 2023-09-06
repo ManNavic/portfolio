b711913c724a09d568929215f63dbae8d8da849a
Mantas Navickis <navickis@gmail.com>
2023-09-06 16:49:09 +0200

    header created

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import './header.css'
+const Header = () =>{
+    return (
+        <div className="header-container">
+            <div className='button-container'>
+                <button>About Me</button>
+            <button>My projects</button>
+            <button>Contact Me</button>
+            </div>
+            
+        </div>
+    )
+}
+export default Header
