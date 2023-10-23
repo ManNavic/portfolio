a813a91404a1ce3bd1ca090bccc49db206d59dc5
Mantas Navickis <navickis@gmail.com>
2023-10-23 10:57:50 +0200

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
-import Header from './components/header';
+
 
 import About from './components/about';
+import Contact from './components/contact';
 import Main from './components/main';
+import Projects from './components/projects';
 
 function App() {
   return (
     <div className="app-background">
-      <Header/>
-      <Main/>
+      <Main />
       <About/>
+      <Projects/>
+      <Contact/>
     </div>
   );
 }
